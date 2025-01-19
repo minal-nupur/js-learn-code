335207594b08a2917ee0363b1faa3db1ac93232f
minal-nupur <54581142+minal-nupur@users.noreply.github.com>
2025-01-19 19:45:12 +0000

    dataTypes code

---
+++ 01-basics/01-dataType.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all the js code as newer version
+
+//alert(3+3); // we are using node.js now browser console
+
+//code Readability is important
+//console.log(3+3) console.log("Minal") // break the line
+console.log(3+3)
+console.log("Minal")
+
+//Datatype
+let name = "Minal"
+let age = 26
+let isLogged = true
+
+//typeof used to find the dataType of any variable
+
+console.log("------------")
+console.log("       ")
+console.log(typeof name)
+console.log(typeof "Minal")
+console.log(typeof age)
+console.log(typeof isLogged)
+console.log(typeof null)
+console.log(typeof undefined)
+
+console.table([typeof name, typeof "Minal", typeof age, typeof isLogged, typeof null, typeof undefined])
